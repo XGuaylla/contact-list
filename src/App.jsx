fd4acb19a8de7b179cf59aaab13bd6dae0811d48
Lemar Levy <lemarlevy@gmail.com>
2023-07-24 14:49:12 -0400

    add new files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import ContactList from './components/ContactList'
 
-function App() {
+import "./App.css";
+import { useState } from "react";
+import ContactList from "./components/ContactList";
+import SelectedContact from "./components/SelectedContact";
+
+export default function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
 
   return (
-    <div>
-      <ContactList />
-    </div>
-  )
+    <>
+      {selectedContactId ? (
+        <div>Selected Contact is Present</div>
+      ) : (
+        <ContactList setSelectedContactId={setSelectedContactId} />
+      )}
+    </>
+  );
 }
 
-export default App
+
+
+
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,45 +1,53 @@
-import { useState, useEffect } from 'react'
+
+import React, { useState, useEffect } from 'react';
 import ContactRow from './ContactRow';
 
 const dummyContacts = [
-    { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
-    { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
-    { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
-  ];
+  { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
+  { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
+  { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
+];
 
-export default function ContactList() { 
-    const [contact, setContact] = useState([]);
-    useEffect(() => {
+export default function ContactList({ setSelectedContactId }) {
+  const [contact, setContact] = useState([]);
+
+  useEffect(() => {
     async function fetchContact() {
-          const response = await fetch(`https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users`)
-          const contact = await response.json()
-          setContact(contact);
+      const response = await fetch(`https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users`);
+      const contact = await response.json();
+      setContact(contact);
     }
-    
-    fetchContact()
-  },[])
 
-  const [contacts, setContacts] = useState(dummyContacts)
-//   console.log("Contacts: ", contacts)
-  return ( 
-        <table>
-          <thead>
-            <tr>
-              <th colSpan="3">Contact List</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Name</td>
-              <td>Email</td>
-              <td>Phone</td>
-            </tr>
-            {
-               contacts.map((contact) => {
-                return <ContactRow key={contact.id} contact={contact} />;
-               })
-            }
-          </tbody>
-        </table>
-    ); 
-}
+    fetchContact();
+  }, []);
+
+  const [contacts, setContacts] = useState(dummyContacts);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th colSpan="3">Contact List</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Name</td>
+          <td>Email</td>
+          <td>Phone</td>
+        </tr>
+        {
+          contacts.map((contact) => {
+            return (
+              <ContactRow
+                key={contact.id}
+                contact={contact}
+                setSelectedContactId={setSelectedContactId}
+              />
+            );
+          })
+        }
+      </tbody>
+    </table>
+  );
+} 
